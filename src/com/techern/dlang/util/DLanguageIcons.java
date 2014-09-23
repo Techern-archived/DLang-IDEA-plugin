b9c8f3d57c7ada404775c8222c975d127b835a2b
Cruz Julian Bishop <cruz@techern.com>
2014-09-24 08:15:51 +1000

    Add some initial icons
    Signed-off-by: Cruz Julian Bishop <cruz@techern.com>

---
+++ src/com/techern/dlang/util/DLanguageIcons.java
@@ -0,0 +1,21 @@
+package com.techern.dlang.util;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * DLanguageIcons - A utility class that holds the different {@link javax.swing.Icon}s
+ *
+ * @since 0.0.1
+ */
+public class DLanguageIcons {
+
+    /**
+     * The temporary blank {@link javax.swing.Icon}
+     *
+     * @since 0.0.1
+     */
+    public static final Icon BLANK_ICON = IconLoader.getIcon("com/techern/dlang/icons/blankCircle.png");
+
+}
